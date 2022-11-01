5040984c3a25cb0ea549d15fd91ab6dad4f691c3
jamesOwO <113694418+jamesOwO@users.noreply.github.com>
2022-11-01 17:19:38 +0000

    added to level

--- 2D game/Assets/Scripts/EnemyControl.cs
+++ 2D game/Assets/Scripts/EnemyControl.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public class EnemyControl : MonoBehaviour
 {
@@ -8,13 +10,31 @@ public class EnemyControl : MonoBehaviour
     public Transform LaunchOffset1;
     public Transform LaunchOffset2;
     public Transform LaunchOffset3;
+    [SerializeField] private Health Playerhealth;
+    [SerializeField] private Enemyhealth Enemyhealth;
+    private float projectile1fire = 15;
+    private float projectile2fire = 15;
+    private float projectile3fire = 20;
+
     void Start()
     {
-        
     }
 
     void Update()
     {
+        Debug.Log(Time.time);
+        if (Time.time > 15)
+        {
+            if (Playerhealth.currenthealth > 0 && Enemyhealth.enemycurrenthealth > 0)
+            {
+                
+                if (Time.time > projectile1fire)
+                {
+                    Instantiate(projectileprefab, LaunchOffset1.position, transform.rotation);
+                }
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.L))
         {
             Instantiate(projectileprefab, LaunchOffset1.position, transform.rotation);
